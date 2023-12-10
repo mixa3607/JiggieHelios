b7170670f7254851c7b2733f5ac111fe1445274b
mixa3607 <30209772+mixa3607@users.noreply.github.com>
2023-12-10 23:43:08 +0500

    ch

--- external-deps/chrome.sh
+++ external-deps/chrome.sh
@@ -3,11 +3,8 @@ set -e
 
 RID="$1" # linux-x64 / win-x64
 
-if ! [ -d "chrome/win64-120.0.6099.71" && "$RID" == "win-x64" ]; then
+if ! [ -d "chrome/win64-120.0.6099.71" ] && [ "$RID" == "win-x64" ]; then
   npx @puppeteer/browsers install chrome@120.0.6099.71 --platform win64
-elif ! [ -d "chrome/linux-120.0.6099.71" && "$RID" == "linux-x64" ]; then
+elif ! [ -d "chrome/linux-120.0.6099.71" ] && [ "$RID" == "linux-x64" ]; then
   npx @puppeteer/browsers install chrome@120.0.6099.71 --platform linux
-else
-  echo unsupported rid
-  exit 1
 fi
