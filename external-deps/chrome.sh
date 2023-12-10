860e31437383be47ad8629d35d7276b76dc4cd66
mixa3607 <30209772+mixa3607@users.noreply.github.com>
2023-12-10 23:41:28 +0500

    ch

--- external-deps/chrome.sh
+++ external-deps/chrome.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 set -e
 
-if ! [ -d "chrome/win64-120.0.6099.71" ]; then
+RID="$1" # linux-x64 / win-x64
+
+if ! [ -d "chrome/win64-120.0.6099.71" && "$RID" == "win-x64" ]; then
   npx @puppeteer/browsers install chrome@120.0.6099.71 --platform win64
-fi
-if ! [ -d "chrome/linux-120.0.6099.71" ]; then
+elif ! [ -d "chrome/linux-120.0.6099.71" && "$RID" == "linux-x64" ]; then
   npx @puppeteer/browsers install chrome@120.0.6099.71 --platform linux
+else
+  echo unsupported rid
+  exit 1
 fi
--- external-deps/ffmpeg.sh
+++ external-deps/ffmpeg.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 set -e
 
-if ! [ -d "ffmpeg/win64" ]; then
+RID="$1" # linux-x64 / win-x64
+
+if ! [ -d "ffmpeg/win64" && "$RID" == "win-x64" ]; then
   mkdir -p ffmpeg/win64
   pushd ffmpeg/win64
   curl https://www.gyan.dev/ffmpeg/builds/ffmpeg-release-full.7z -L > ffmpeg-release-full.7z
   #sudo apt-get install p7zip-full
   7z x ffmpeg-release-full.7z
   popd
-fi
-if ! [ -d "ffmpeg/linux64" ]; then
+elif ! [ -d "ffmpeg/linux64" && "$RID" == "linux-x64" ]; then
   mkdir -p ffmpeg/linux64
   pushd ffmpeg/linux64
   curl https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz -L > ffmpeg-release-amd64-static.tar.xz
   tar -xf ffmpeg-release-amd64-static.tar.xz
   popd
+else
+  echo unsupported rid
+  exit 1
 fi
